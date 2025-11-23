902d8c6693904090782300d355e0e16f193a4096
Huiyeongkim <huiyeong9619@naver.com>
2025-11-23 21:42:50 +0900

    feat: PurchaseOrder 엔티티에 스웨거 스키마 추가

--- src/main/java/com/example/homework/order/domain/PurchaseOrder.java
+++ src/main/java/com/example/homework/order/domain/PurchaseOrder.java
@@ -1,5 +1,6 @@
 package com.example.homework.order.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -9,34 +10,43 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Schema(description = "구매 주문 엔티티")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "\"purchase_order\"", schema = "public")
 public class PurchaseOrder {
 
+    @Schema(description = "주문 고유 식별자")
     @Id
     private UUID id;
 
+    @Schema(description = "상품 고유 식별자")
     @Column(name = "product_id", nullable = false)
     private UUID productId;
 
+    @Schema(description = "판매자 고유 식별자")
     @Column(name = "seller_id", nullable = false)
     private UUID sellerId;
 
+    @Schema(description = "구매자 고유 식별자")
     @Column(name = "member_id", nullable = false)
     private UUID memberId;
 
+    @Schema(description = "주문 금액")
     @Column(nullable = false, precision = 15, scale = 2)
     private BigDecimal amount;
 
+    @Schema(description = "주문 상태")
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private PurchaseOrderStatus status;
 
+    @Schema(description = "주문 생성 일시")
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
+    @Schema(description = "주문 수정 일시")
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
